a3cbc258b81dc1d63e09f69a05e89e36811f84c3
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-08-25 11:05:22 +0300

    FINISHED SQL

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES
+ ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+ ('Rowdy', 3, 'M', 'Black', 'Energetic', 1),
+ ('Tabitha', 2, 'F', 'White', 'Playful', 1),
+ ('Sergeant Brown', 6, 'M', 'Brown', 'Grumpy', 1),
+ ('Melissa', 4, 'F', 'Honey', 'Calm', 1),
+ ('Grinch', 7, 'M', 'Green', 'Mischievous', 1),
+ ('Wendy', 1, 'F', 'Gray', 'Adventurous', 1),
+ (NULL, NULL, NULL, NULL, NULL, 0);
